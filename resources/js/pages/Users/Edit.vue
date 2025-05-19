2c7f61d3fe5b0c7c4a0a1348aa4be199eb0cb6f0
Fran <francisco20990@gmail.com>
2025-05-18 22:47:26 -0400

    fix

--- resources/js/pages/Users/Edit.vue
+++ resources/js/pages/Users/Edit.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import FormEdit from './Components/FormEdit.vue';
 
